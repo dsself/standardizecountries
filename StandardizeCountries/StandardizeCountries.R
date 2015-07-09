cd9b8b2a9dd934aa1b5b4ddb23c28b3f74d632cc
dsself <dsself@wm.edu>
2015-07-08 20:15:47 -0400

    Correct South Vietnam standardization

--- StandardizeCountries/StandardizeCountries.R
+++ StandardizeCountries/StandardizeCountries.R
@@ -1,15 +1,10 @@
 setwd("C:/Users/darin/Documents/StandardCountries")
+library(readr)
+library(devtools)
 ccodes <- read_csv("StandardizeCountries/CountryCodes.csv")
 cnames <- read_csv("StandardizeCountries/CountryNames.csv")
 
-country_code <- function(x) {
-  ccodes[match(x,ccodes$country),]$id
-}
-
-country_name <- function(x) {
-  cnames[match(x,cnames$country),]$standardize
-}
 
 setwd("R")
-devtools::use_data(cnames, ccodes, internal = TRUE)
+use_data(cnames, ccodes, internal = TRUE, overwrite = TRUE)
 

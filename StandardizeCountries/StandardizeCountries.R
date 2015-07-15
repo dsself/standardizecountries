3a56e99b2857317a96890a01141cf329e4329a49
dsself <dsself@wm.edu>
2015-07-15 16:01:23 -0400

    Improve panel code to remove nested ifelse statements

--- StandardizeCountries/StandardizeCountries.R
+++ StandardizeCountries/StandardizeCountries.R
@@ -11,7 +11,15 @@ d2 <- ddply(d1, c("country"), function(DF) {
  DF
 })
 
-cpanel <- unite(d2, merged, country, year, sep = ".") 
+write_csv(d2, "StandardizeCountries/CountryPanel.csv")
+
+d3 <- d2 %>%
+  mutate(standardize = ifelse(standardize == "Vietnam, Democratic Republic of" & year > 1975, "Vietnam", standardize)) %>%
+  mutate(standardize = ifelse(standardize == "Yemen (Arab Republic of Yemen)" & year > 1989, "Yemen, Republic of", standardize)) %>%
+  mutate(standardize = ifelse(standardize == "Czech Republic" & year > 1917 & standardize == "Czech Republic" & year < 1993, "Czechoslovakia", standardize)) 
+
+
+cpanel <- unite(d3, merged, country, year, sep = ".") 
 
 setwd("R")
 use_data(cnames, ccodes, cpanel, internal = TRUE, overwrite = TRUE)
